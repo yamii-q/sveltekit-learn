894a1e983a28be0d539f4d08ecc6975f45256f46
yamee-dev <ryo.eiyama@icloud.com>
2022-10-31 16:30:33 +0900

    API呼び出し処理

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,13 +1,45 @@
 <script lang="ts">
   import SearchBar from "../components/SearchBar.svelte";
+  import type { BookItem, Result } from "../repositories/book";
+  import RepositoryFactory, { BOOK } from "../repositories/RepositoryFactory";
+  const BookRepository = RepositoryFactory[BOOK];
 
   let q = "JavaScript";
+  let empty = false;
+  let books: BookItem[] = [];
+  let promise: Promise<void>;
+
+  const handleSubmit = () => {
+    if (!q.trim()) return;
+    promise = getBooks();
+  };
+
+  const getBooks = async () => {
+    books = [];
+    empty = false;
+    const result = await BookRepository.get({ q });
+    empty = result.totalItems === 0;
+    books = result.items;
+  };
 </script>
 
-<form>
+<form on:submit|preventDefault={handleSubmit}>
   <SearchBar bind:value={q} />
 </form>
 
 <div class="text-center mt-4">
-  {q}
+  {#if empty}
+    <div>検索結果が見つかりませんでした</div>
+  {:else}
+    {#each books as book (book.id)}
+      <div>{book.volumeInfo.title}</div>
+    {/each}
+  {/if}
+  {#await promise}
+    <div>loading...</div>
+  {:catch e}
+    <span class="text-red-600 text-sm">
+      {e.message}
+    </span>
+  {/await}
 </div>
