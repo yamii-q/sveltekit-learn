aab8ba09131e10644a7ac43645111d6a7ec58697
yamee-dev <ryo.eiyama@icloud.com>
2022-11-07 23:43:46 +0900

    詳細ページ追加

---
+++ src/pages/DetailsBook.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import type { BookItem } from "../repositories/book";
+  import type { Readable } from "svelte/store";
+  import { books, find } from "../../store/book";
+  import RepositoryFactory, { BOOK } from "../repositories/RepositoryFactory";
+  import Spinner from "../components/Spinner.svelte";
+  const BookRepository = RepositoryFactory[BOOK];
+
+  type Params = { id: string };
+  export let params: Params;
+
+  let book: Readable<BookItem>;
+  let promise: Promise<void>;
+
+  const findById = async (id: string) => {
+    const book = await BookRepository.find(id);
+    books.add([book]);
+  };
+
+  book = find(params.id);
+  if (!$book) {
+    promise = findById(params.id);
+  }
+</script>
+
+<div>
+  {#await promise}
+    <div class="flex justify-center">
+      <Spinner />
+    </div>
+  {:then}
+    {$book.volumeInfo.title}
+  {:catch e}
+    <span class="text-red-600 text-sm">
+      {e.message}
+    </span>
+  {/await}
+</div>
