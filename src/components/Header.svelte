0a90c63d2bec81c3c0a491c0aa7af650b29cda11
yamee-dev <ryo.eiyama@icloud.com>
2022-11-26 17:15:10 +0900

    headerにリンク追加

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,8 +2,10 @@
   class="top-0 lef-0 w-full z-40 bg-gray-900 shadow fixed border-b border-gray-200"
 >
   <div class="container mx-auto px-6 h-16 flex justify-between items-center">
-    <span class="font-semibold text-xl text-white tracking-tight">
-      ブックレビュー App
-    </span>
+    <a href="/" class="no-underline">
+      <span class="font-semibold text-xl text-white tracking-tight">
+        ブックレビュー App
+      </span>
+    </a>
   </div>
 </header>
