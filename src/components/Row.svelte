457f653d985b4bfde2f30c43a1f2335edc807617
yamee-dev <ryo.eiyama@icloud.com>
2022-11-26 13:38:52 +0900

    コンポーネント修正

--- src/components/Row.svelte
+++ src/components/Row.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div
-  class="bg-gray-50 border-b border-gray-200 px-4 py-5 sm:grid-cols-3 sm:gap-4 sm:px-6"
+  class="bg-gray-50 border-b border-gray-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6"
 >
-  <dt class="text-sm font-medium">
+  <dt class="text-sm font-medium text-gray-500">
     {dt}
   </dt>
   <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
